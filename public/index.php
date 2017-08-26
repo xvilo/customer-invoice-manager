0b62882028a5a280bf26b086d3ee30ca53a9a82f
Sem Schilder <sem@tropical.email>
2017-08-26 13:56:56 +0200

    Add basic files 👍

---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+
+// Logic here!
---
+++ settings-sample.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Settings file. Adjust accordingly!
+
+$settings = [
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'database_user',
+        'password' => 'P@ssw00rd',
+        'database' => 'database_name',
+    ],
+    'use-cache' => true,
+];
