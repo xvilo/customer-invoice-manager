247440d31b6b1d0f9fdfc4844ea230da2f41ca9f
Sem Schilder <sem@tropical.email>
2017-08-26 14:37:52 +0200

    Loading all needed files 🤗

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-// Logic here!
+// Autoloading all composer packages
+require_once __DIR__.'../vendor/autoload.php';
+
+// Autoloading all customer classes
+require_once __DIR__.'../private/autoload.php';
+
+// Get basic settings
+require_once __DIR__.'../settings.php';
