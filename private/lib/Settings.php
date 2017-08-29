463b2f14a52bafdf48b7b08085df618124382145
Sem Schilder <sschilder@transip.nl>
2017-08-29 18:21:55 +0200

    Ignoring correct file and add Settings class

---
+++ private/lib/Settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cim - A simple invoice manager
+ *
+ * @author      Sem Schilder <sem@tropical.email>
+ * @copyright   (c) Sem Schilder
+ * @link        https://github.com/xvilo/customer-invoice-manager
+ */
+
+/**
+ * Class Settings
+ */
+class Settings
+{
+    
+}
